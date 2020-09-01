2e8a9c0aaa099a510d8c737d779ebebed61ca17c
gradulescu <gradulescu@seedbox.com>
2020-09-01 08:00:20 -0400

    Updates

---
+++ precache-manifest.41d5a879272c8aaff19c6a48b409f08e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ae80f8b18ddae60089c167a00513d3",
+    "url": "/stockapp/index.html"
+  },
+  {
+    "revision": "9617536ebcfbde1e8b86",
+    "url": "/stockapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd2eefdbb38ac8a3c62d",
+    "url": "/stockapp/static/js/2.33f949c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stockapp/static/js/2.33f949c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9617536ebcfbde1e8b86",
+    "url": "/stockapp/static/js/main.c6d263c2.chunk.js"
+  },
+  {
+    "revision": "7de1dd382603d87ccd09",
+    "url": "/stockapp/static/js/runtime-main.35b4e246.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stockapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
